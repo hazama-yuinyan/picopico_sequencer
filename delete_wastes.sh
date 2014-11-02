32be8f3a44c16032f712e8ccb9a789a9242342a1
HAZAMA <kotonechan@live.jp>
2014-11-02 18:13:23 +0900

    update version info and slightly revise the manual

--- delete_wastes.sh
+++ delete_wastes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-DEST="$HOME/workspace/PicopicoSequencer/war"
-DEST_PARENT="$HOME/workspace/PicopicoSequencer"
+DEST="$HOME/workspace/PicopicoSequencerGAE/picopico-seq/src/main/webapp"
+DEST_PARENT="$HOME/workspace/PicopicoSequencerGAE/picopico-seq/src/main"
 EXCLUDES="treehugger|dojox"
 
 echo "Cleaning the intermmediate files..."
-find release -name "*.js.*" -delete
+#find release -name "*.js.uncompressed.js" -delete
+find release -name "*.js.consoleStripped.js" -delete
 echo "Copying built files to DEST..."
+# Move WEB-INF and favicon.ico to the parent directory of the destnation.
 mv "$DEST/WEB-INF" "$DEST_PARENT"
 mv "$DEST/favicon.ico" "$DEST_PARENT"
 mv release/build-report.txt .
@@ -18,12 +20,13 @@ SOURCES=(`ls release`)
 for path in ${SOURCES[@]} ; do
     TMP=`echo $path | grep -E $EXCLUDES -`
     if [ -z "$TMP" ] ; then
-	cp -Rvu "release/$path" "$DEST"
+		cp -Rvu "release/$path" "$DEST"
     fi
 done
 
 mkdir "$DEST/dojox"
 cp -Ruv "release/dojox/gfx" "$DEST/dojox"
 
+# Move back WEB-INF and favicon.ico to the destination directory.
 mv "$DEST_PARENT/WEB-INF" "$DEST"
 mv "$DEST_PARENT/favicon.ico" "$DEST"
